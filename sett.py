95b4f3c9477adf794910d9c666010f4529644b52
juampi1001 <123843228+juampi1001@users.noreply.github.com>
2024-04-23 13:43:59 -0300

    Update sett.py

--- sett.py
+++ sett.py
@@ -1,7 +1,1 @@
-verify_token = 'tourism'
 
-whatsapp_api_token = 'EAAKizFVIYUIBOZC3bdQXjqfSBGzcBYqvcySDJrlEbdUiQVZA3M3eErTQgpukJiRV07xh6Ewi2P3nMFwKJZA7Tr3cJQiZCPsDfsBHPEgTDDFlrx6KVIPAlCITeL5ZBZBpVvT1IqfQN4rmWuvGfZByjkBY30qKUUboJcLwIMMnDNkAJWnCg2OK0zsMSWcZBchUQ02MZA9bnEZCFnpNyWYuQoPp4ZD'
-
-whatsapp_url = 'https://graph.facebook.com/v18.0/261625013703699/messages'
-
-#genai_key = 'AIzaSyBieZc4Jq22TUxnkCTrdZ4RI5uzQ2GFsGQ'
